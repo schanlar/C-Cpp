f1de170b7eb63c3e24d846c4872b6919055fc922
Savvas Chanlaridis <schanlaridis@gmail.com>
2023-04-05 05:09:04 +0300

    functions to find and remove a number from list added

--- C/arrayList.c
+++ C/arrayList.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <float.h>
+
 
 typedef struct list_properties{
     double *array;
@@ -7,10 +11,36 @@ typedef struct list_properties{
 }List;
 
 
+int contain(List l, double num, double eps){
+    // serial search
+    for (int idx = 0; idx < l.size; idx++){
+        if (fabs(l.array[idx] - num) <= eps){
+            return idx;
+        }
+    }
+    return -1;
+}
+
 List insert(List l, double num){
     l.array = (double *) realloc(l.array, sizeof(double) * (l.size + 1));
     l.array[l.size] = num;
     (l.size)++;
+    return l;
+}
+
+List extract(List l, double num){
+    /* index of number in list (-1 if number does not exist) */
+    int idx = contain(l, num, 1e-6);
+    /* If number exists in List then move elements one position back */
+    if (idx != -1){
+        for (int i = idx; i < l.size - 1; i++){
+            int i_next = i + 1;
+            l.array[i] = l.array[i_next];
+        }
+        /* Re-allocate memory and update size */
+        l.array = (double *) realloc(l.array, sizeof(double) * (l.size - 1));
+        (l.size)--;
+    }
     return l;
 }
 
@@ -21,12 +51,31 @@ void append(List *pl, double num){
     return;
 }
 
+double pop(List *pl, double num){
+    /* index of number in list (-1 if number does not exist) */
+    int idx = contain(*pl, num, 1e-6);
+    /* If number exists in List then move elements one position back */
+    if (idx != -1){
+        double popped_value = (pl -> array [idx]);
+        for (int i = idx; i < (pl -> size - 1); i++){
+            int i_next = i + 1;
+            pl -> array[i] = (pl -> array[i_next]);
+        }
+        /* Re-allocate memory and update size */
+        pl -> array = (double *) realloc(pl -> array, sizeof(double) * (pl -> size - 1));
+        (pl -> size)--;
+        
+        return popped_value;
+    }
+    return -DBL_MAX;
+}
+
 void freeList(List *pl){
     if (pl == NULL){
         return;
     }
-    else{
-        free(pl -> array);
+    else {
+        free(pl -> array); // De-allocate the part of the structure that is allocated in heap memory
         pl = NULL;
         return;
     }
@@ -39,22 +88,41 @@ void display(List l){
         printf(s, l.array[i]);
     }
     printf("]\n");
+    return;
 }
 
 
-
 int main(void){
-    List my_list;
+    srand(time(NULL));
+    
+    /* 
+    * This is a variable stored in stack memory.
+    * Within the stack frame there is a pointer (my_list.array) 
+    * that points to the allocated memory in heap where the array is defined. 
+    * In other words, the pointer "my_list.array" is stored in stack
+    * but it points to the heap memory where the actual data are stored.
+    */
+    List my_list; 
     my_list.size = 0;
-
+    
+    /* Add random numbers using the append function */ 
     for (int i = 1; i < 5; i++){
-        append(&my_list, i);
+        append(&my_list, i + (rand() % 10) / 10.);
     }
     display(my_list);
     
-    for (int j = 10; j < 13; j++){
+    /* Add numbers using the insert function */
+    for (int j = 10; j < 12; j++){
         my_list = insert(my_list, j);
     }
+    display(my_list);
+    
+    /* Remove number using the pop function */
+    double tmp = pop(&my_list, 10.000001);
+    display(my_list);
+    
+    /* Remove number using the extract function */
+    my_list = extract(my_list, 11.0);
     display(my_list);
     
     freeList(&my_list);
